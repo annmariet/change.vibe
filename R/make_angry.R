fef996d977dca1e5aeec26feae39341c3dad7bee
annmariet <annmariethomson.1@gmail.com>
2025-04-04 13:25:05 -0700

    Added sad function!

--- R/make_angry.R
+++ R/make_angry.R
@@ -1,3 +1,13 @@
+#' Make your string angry
+#'
+#' @param string a string to make angry.
+#'
+#' @return A string.
+#' @export
+#'
+#' @examples
+#' x <- "hello"
+#' make_angry(x)
 make_angry <- function(string) {
   toupper(string)
 }
---
+++ R/make_sad.R
@@ -0,0 +1,13 @@
+#' Make your string sad
+#'
+#' @param string a string to make sad
+#'
+#' @return A string.
+#' @export
+#'
+#' @examples
+#' x <- "Hello"
+#' make_sad(x)
+make_sad <- function(string) {
+  tolower(string)
+}
